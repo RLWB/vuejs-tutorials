60d8bb24e9430f15af10fa7c9883887a1a1090db
rlwb <rlwb1017669154@gmail.com>
2017-05-25 23:58:47 +0800

    come up 80%,there are also someting wrong in router children to come over

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by RLWB on 2017/5/25.
+ */
+// src/store/index.js
+import Vue from 'vue'
+import Vuex from 'vuex'
+import mutations from './mutations'
+import actions from './actions'
+
+Vue.use(Vuex);
+
+const state = {
+    totalTime: 0,
+    list: []
+};
+
+export default new Vuex.Store({
+    state,
+    mutations,
+    actions
+})
